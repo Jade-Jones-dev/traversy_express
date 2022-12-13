6261cc1e60f7255e2c3e50e4ce0493b2d2a483ad
Jade Jones <jadejonescrypto@gmail.com>
2022-12-13 21:13:49 +0000

    up to 37.38

---
+++ Member.js
@@ -0,0 +1,24 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        staus: 'active'
+    },
+    {
+        id: 2,
+        name: 'George Doe',
+        email: 'george@gmail.com',
+        staus: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Jane Doe',
+        email: 'john@gmail.com',
+        staus: 'inactive'
+    }
+
+
+]
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const path = require('path');
+const members = require('./Member');
+const logger = require('./middleware/logger')
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('Hello World')
+// init middleware
+// app.use(logger);
+
+// Gets All members
+app.get('/api/members', (req, res) => res.json(members)
+);
+
+// get single member
+app.get('/api/members/:id', (req, res) =>{
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if(found){
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    } else {
+        res.status(400).json({ msg : `no member with the id of ${req.params.id}`})
+    }
+    
 })
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const PORT = process.env.PORT || 5000;
 
