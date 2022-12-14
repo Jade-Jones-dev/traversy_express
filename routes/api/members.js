9d49bcca2a978341d45f807c88024a51422350d2
Jade Jones <jadejonescrypto@gmail.com>
2022-12-14 09:44:13 +0000

    add members

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const uuid = require('uuid')
 const  router = express.Router();
 const members = require('../../Member');
 // Gets All members
@@ -16,6 +17,24 @@ router.get('/:id', (req, res) =>{
     }
     
 });
+
+// create member
+router.post('/' , (req, res) => {
+    const newMember = {
+        id : uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+
+    if(!newMember.name || !newMember.email){
+        res.status(400).json({ message: 'Please include a name and email'})
+    }
+    members.push(newMember);
+    res.json(members)
+});
+
+// 
 
 module.exports = router;
 
