3faacc560b3ca17ace71e2a7764057cbe4b72457
Jade Jones <jadejonescrypto@gmail.com>
2022-12-14 09:23:18 +0000

    routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Member');
+
 const logger = require('./middleware/logger')
 
 const app = express();
@@ -8,23 +8,11 @@ const app = express();
 // init middleware
 // app.use(logger);
 
-// Gets All members
-app.get('/api/members', (req, res) => res.json(members)
-);
-
-// get single member
-app.get('/api/members/:id', (req, res) =>{
-    const found = members.some(member => member.id === parseInt(req.params.id));
-
-    if(found){
-        res.json(members.filter(member => member.id === parseInt(req.params.id)))
-    } else {
-        res.status(400).json({ msg : `no member with the id of ${req.params.id}`})
-    }
-    
-})
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// members api routes
+app.use('/api/members', require('./routes/api/members'))
 
 
 const PORT = process.env.PORT || 5000;
